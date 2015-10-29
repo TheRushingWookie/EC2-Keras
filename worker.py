173a1bded2614ae9e88e7e4437fe10d73c21b5b3
Quinn Jarrell <quinnjarr@gmail.com>
2015-10-29 17:07:14 -0500

    fix typo

--- worker.py
+++ worker.py
@@ -55,7 +55,7 @@ def shutdown_spot_request():
     self_id = requests.get('http://instance-data/latest/meta-data/public-ipv4').text
     spot_reqs = ec2conn.get_all_spot_instance_requests()
     for spot_req in spot_reqs:
-        if spot_req.instance_id = self_id:
+        if spot_req.instance_id == self_id:
             ec2conn.cancel_spot_instance_requests([spot_req.id,])
             ec2conn.stop_instances(instance_ids=[self_id,])
             return
